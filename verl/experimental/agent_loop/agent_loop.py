737757592a61c5b3de0e2d0c6ad5eda56f287694
Fei Tang <fei.tang3@gmail.com>
2026-01-23 08:44:59 +0100

    run precommit format

--- verl/experimental/agent_loop/agent_loop.py
+++ verl/experimental/agent_loop/agent_loop.py
@@ -37,6 +37,7 @@ from verl.protocol import DataProto
 from verl.single_controller.ray.base import RayResourcePool, RayWorkerGroup
 from verl.utils import hf_processor, hf_tokenizer
 from verl.utils.fs import copy_to_local
+from verl.utils.import_utils import import_external_libs
 from verl.utils.model import compute_position_id_with_mask
 from verl.utils.rollout_trace import (
     RolloutTraceConfig,
@@ -45,7 +46,6 @@ from verl.utils.rollout_trace import (
 )
 from verl.utils.transferqueue_utils import tqbridge
 from verl.workers.rollout.replica import TokenOutput, get_rollout_replica_class
-from verl.utils.import_utils import import_external_libs
 
 logger = logging.getLogger(__file__)
 logger.setLevel(os.getenv("VERL_LOGGING_LEVEL", "WARN"))
@@ -263,7 +263,9 @@ class AgentLoopWorkerBase:
         if not hasattr(self, "server_manager"):
             self.server_manager = AsyncLLMServerManager(config, server_handles)
 
-        import_external_libs(config.actor_rollout_ref.model.get("external_lib", None))  # need the external libs for tokenizer/processor
+        import_external_libs(
+            config.actor_rollout_ref.model.get("external_lib", None)
+        )  # need the external libs for tokenizer/processor
         self.reward_router_address = reward_router_address
 
         model_path = config.actor_rollout_ref.model.path
